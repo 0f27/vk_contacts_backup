8b2aa3c3154c62a7dbf0dd7341c8e4a754a38247
0f27 <x0f27@yahoo.com>
2016-11-21 02:08:20 +0300

    script upload

---
+++ vk_contacts_backup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#-*- coding: UTF-8 -*-
+#
+import vk,sys,csv
+
+session = vk.AuthSession(app_id=sys.argv[1],user_login=sys.argv[2],user_password=sys.argv[3])
+api = vk.API(session)
+friend_ids = api.friends.get()
+contacts = api.users.get(user_ids=friend_ids,fields='bdate,contacts')
+
+with open('contacts_vk.csv', 'w', encoding='utf-8') as f:
+    w = csv.DictWriter(f, ['uid', 'first_name', 'last_name', 'home_phone', 'mobile_phone', 'bdate', 
+'deactivated'])
+    w.writeheader()
+    for i in contacts:
+        w.writerow(i)
