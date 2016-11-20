03a1b12887116d64098a7831158e96c4ff2b5a27
0f27 <x0f27@yahoo.com>
2016-11-21 02:50:11 +0300

    added fields: sex and photo

--- vk_contacts_backup.py
+++ vk_contacts_backup.py
@@ -6,11 +6,11 @@ import vk,sys,csv
 session = vk.AuthSession(app_id=sys.argv[1],user_login=sys.argv[2],user_password=sys.argv[3])
 api = vk.API(session)
 friend_ids = api.friends.get()
-contacts = api.users.get(user_ids=friend_ids,fields='bdate,contacts')
+contacts = api.users.get(user_ids=friend_ids,fields='bdate,contacts,sex,photo_max')
 
 with open('contacts_vk.csv', 'w', encoding='utf-8') as f:
-    w = csv.DictWriter(f, ['uid', 'first_name', 'last_name', 'home_phone', 'mobile_phone', 'bdate', 
-'deactivated'])
+    w = csv.DictWriter(f, ['uid', 'first_name', 'last_name', 'sex', 'home_phone', 'mobile_phone', 
+'bdate', 'photo_max', 'deactivated'])
     w.writeheader()
     for i in contacts:
         w.writerow(i)
